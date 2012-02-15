acc1ad009d4b9003e4d2cf24a13d5e420ec6bd03
Wess Cope <wcope@me.com>
2012-02-15 17:57:05 -0500

    updated header

--- Additions.h
+++ Additions.h
@@ -6,14 +6,13 @@
 //
 
 #import "UIImage+Additions.h"
-#import "UIColor+Additions.h"
+#import "UIControl+Additions.h"
 #import "UIButton+Additions.h"
 #import "NSDate+Additions.h"
 #import "NSArray+Additions.h"
 #import "NSData+Base64.h"
 #import "NSDictionary+Additions.h"
 #import "NSString+Additions.h"
-#import "UIButton+Additions.h"
 #import "UIColor+Additions.h"
 #import "UIGestureRecognizer+Additions.h"
 #import "UIView+Additions.h"
@@ -23,6 +22,3 @@
 #import "NSFetchRequest+Additions.h"
 #import "NSManagedObject+Additions.h"
 #import "Constants.h"
-#import "NSFetchRequest+Additions.h"
-#import "NSManagedObject+Additions.h"
-#import "NSManagedObjectContext+Additions.h"
